74c16f430d29d9f3f39352b87fceb1208efdcd08
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-04-13 23:36:01 -0400

    update to work with webpack 2

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ module.exports = function (source) {
     this.cacheable && this.cacheable(true);
     
     // get config
-    let config = utils.getLoaderConfig(this, "pug");
-    
-    // fill out anything else needed in the config
-    config.filename = this.resourcePath;
-    config.doctype = "html";
-    config.compileDebug = false;
-    config.pretty = true;
+    let config = utils.getLoaderConfig(this);
 
-    // we can overload the webpack options object to share locals across files
-    // if thats not present, then lets go ahead and just grab any configuration locals
-    config.locals = utils.getLoaderConfig(this, "locals");
-    config.basedir = config.locals.basedir;
+    // fill out anything else needed in the config
+    config.filename = this.resourcePath
+    config.doctype = config.doctype || "html"
+    config.compileDebug = config.compileDebug || false
+    config.pretty = config.pretty || true
+    
+    config.basedir = config.locals.basedir
 
     let tmplFunc = "";
     
+
     // if data, then we are using markdown files
     if ( source.data ){
         config.locals.page = source.data;
@@ -33,6 +31,8 @@ module.exports = function (source) {
             config.filename = `${source.data.slug}.html`;
             this.resourcePath = `${source.data.slug}`;
         }
+
+        
         tmplFunc = pug.compile(source.data.template, config);
         tmplFunc.dependencies.map( this.addDependency.bind(this) );
         // we need to return the rendered template so we can output it
@@ -41,10 +41,11 @@ module.exports = function (source) {
     }
     else {
         // compile the template to a function
-        tmplFunc = pug.compile(source, config);
-        tmplFunc.dependencies.map( this.addDependency.bind(this) );
+        tmplFunc = pug.compile(source, config)
+        // lets make it so that included files drive a re-render of parent files
+        tmplFunc.dependencies.map( this.addDependency.bind(this) )
         // we need to return the rendered template so we can output it
-        source = tmplFunc(config.locals);
+        source = tmplFunc(config.locals)
     }
     
     return source;
