87cd24458e622a50fed46b2efd06a672ceeba5b1
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-06-29 22:05:12 -0400

    process only the files you want emitted

--- index.js
+++ index.js
@@ -7,20 +7,26 @@ module.exports = function (source) {
     
     // get config
     let config = utils.getOptions(this);
-
+    
     // fill out anything else needed in the config
     config.filename = this.resourcePath
     config.doctype = config.doctype || "html"
     config.compileDebug = config.compileDebug || false
-    config.self = config.self || true
     config.debug = config.debug || false
-    config.pretty = config.pretty || true
-    config.cache = config.cache || true
+    config.pretty = config.pretty || false
+    config.self = config.self || false
+    config.cache = config.cache || false
     
     config.basedir = config.locals.basedir
 
-    let tmplFunc = "";
-    
+
+    let context = {
+        context: config.context,
+        content: source.content ? source.content : source
+    }
+    let tmplFunc = ""
+    // console.log(context.content)
+    // console.log(config.emit)
 
     // if data, then we are using markdown files
     if ( source.data ){
@@ -44,12 +50,43 @@ module.exports = function (source) {
         source.resourcePath = this.resourcePath;
     }
     else {
-        // compile the template to a function
-        tmplFunc = pug.compile(source, config)
-        // lets make it so that included files drive a re-render of parent files
-        tmplFunc.dependencies.map( this.addDependency.bind(this) )
-        // we need to return the rendered template so we can output it
-        source = tmplFunc(config.locals)
+        let process = true
+
+        if (config.emit) {
+            // only process the files we want to emit
+            
+            // break up the file path into the parts that we need
+            let req_parts = utils.interpolateName(
+                this,
+                "[folder]!![path]!![name]!![ext]",
+                context
+            )
+            
+            const [folder, process_path, file, file_ext] = req_parts.split('!!')
+            let process_path_parts = process_path.split("/")
+            
+            let parts_check = config.emit.indexOf(process_path_parts[0]);
+            
+            // check for a full path
+            let path_check = config.emit.indexOf(process_path);
+            
+            // check for full path plus name
+            let deep_file_check = config.emit.indexOf(`${process_path}${file}`);
+            let file_check = config.emit.indexOf(`${file}.${file_ext}`);
+            
+            if ( parts_check < 0 && path_check < 0 && deep_file_check < 0 && file_check < 0) {
+                process = false
+            } 
+        }
+        
+        if (process) {
+            // compile the template to a function
+            tmplFunc = pug.compile(source, config)
+            // lets make it so that included files drive a re-render of parent files
+            tmplFunc.dependencies.map( this.addDependency.bind(this) )
+            // we need to return the rendered template so we can output it
+            source = tmplFunc(config.locals)
+        }
     }
     
     return source;
